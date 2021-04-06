36514a30f319a5ee0d4b3675a33d7f3d08d8166c
konianzero <konianzero@gmail.com>
2021-04-06 11:46:29 +0300

    Add script for populate companies table

---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,7 @@
+DELETE FROM employees;
+DELETE FROM companies;
+ALTER SEQUENCE global_seq RESTART WITH 1000;
+
+INSERT INTO companies (name, tin, address, phone_number)
+VALUES ('Компания_1', '1234123412', 'Улица 1', '+79005554466'),
+       ('Компания_2', '5678567856', 'Проспект 2', '+75008887799');
