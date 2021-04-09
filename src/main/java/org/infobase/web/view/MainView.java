9f4d715ddce04eb1169b41d0045016e3fb059564
konianzero <konianzero@gmail.com>
2021-04-09 12:20:29 +0300

    Add Tabs

--- src/main/java/org/infobase/web/view/MainView.java
+++ src/main/java/org/infobase/web/view/MainView.java
@@ -1,6 +1,12 @@
 package org.infobase.web.view;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import org.infobase.model.Company;
+import org.infobase.model.Employee;
 import org.infobase.service.CompanyService;
 
 import com.vaadin.flow.component.button.Button;
@@ -10,43 +16,71 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import org.infobase.service.EmployeeService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Route
 public class MainView extends VerticalLayout {
 
     private final CompanyService companyService;
+    private final EmployeeService employeeService;
 
-    final Grid<Company> grid;
+    final Grid<Company> companyGrid;
+    final Grid<Employee> employeeGrid;
 
-    final TextField filter;
-
-    private final Button addBtn;
-    private final Button editBtn;
-    private final Button delBtn;
-
-    public MainView(CompanyService companyService) {
+    public MainView(CompanyService companyService, EmployeeService employeeService) {
         this.companyService = companyService;
+        this.employeeService = employeeService;
 
-        this.grid = new Grid<>();
-        this.filter = new TextField("", "Поиск");
-        this.addBtn = new Button("Добавить", VaadinIcon.PLUS.create());
-        this.editBtn = new Button("Редактировать", VaadinIcon.PENCIL.create());
-        this.delBtn = new Button("Удалить", VaadinIcon.MINUS.create());
+        TextField filter = new TextField("", "Поиск");
+        Button addBtn = new Button("Добавить", VaadinIcon.PLUS.create());
+        Button editBtn = new Button("Редактировать", VaadinIcon.PENCIL.create());
+        Button delBtn = new Button("Удалить", VaadinIcon.MINUS.create());
 
         HorizontalLayout actions = new HorizontalLayout(addBtn, editBtn, delBtn, filter);
-        add(actions, grid);
 
-        grid.setHeight("300px");
-        grid.addColumn(Company::getId).setHeader("ID");
-        grid.addColumn(Company::getName).setHeader("Название");
-        grid.addColumn(Company::getTin).setHeader("ИНН");
-        grid.addColumn(Company::getAddress).setHeader("Адрес");
-        grid.addColumn(Company::getPhoneNumber).setHeader("Телефонный номер");
+        this.companyGrid = new Grid<>();
+        companyGrid.setHeight("300px");
+        companyGrid.addColumn(Company::getId).setHeader("ID");
+        companyGrid.addColumn(Company::getName).setHeader("Название");
+        companyGrid.addColumn(Company::getTin).setHeader("ИНН");
+        companyGrid.addColumn(Company::getAddress).setHeader("Адрес");
+        companyGrid.addColumn(Company::getPhoneNumber).setHeader("Телефонный номер");
 
-        setItemsToGrid();
+        this.employeeGrid = new Grid<>();
+        employeeGrid.setHeight("300px");
+        employeeGrid.addColumn(Employee::getId).setHeader("ID");
+        employeeGrid.addColumn(Employee::getName).setHeader("ФИО");
+        employeeGrid.addColumn(Employee::getBirthDate).setHeader("Дата Рождения");
+        employeeGrid.addColumn(Employee::getEmail).setHeader("Электронная поста");
+
+        Tab companyTab = new Tab("Компании");
+        Tab employeeTab = new Tab("Сотрудники");
+
+        Div companyPage = new Div(companyGrid);
+        Div employeePage = new Div(employeeGrid);
+
+        Map<Tab, Component> tabsToPages = new HashMap<>();
+        tabsToPages.put(companyTab, companyPage);
+        tabsToPages.put(employeeTab, employeePage);
+
+        Tabs tabs = new Tabs(companyTab, employeeTab);
+        Div pages = new Div(companyPage, employeePage);
+
+        tabs.addSelectedChangeListener(event -> {
+            tabsToPages.values().forEach(page -> page.setVisible(false));
+            setItemsToGrid();
+            Component selectedPage = tabsToPages.get(tabs.getSelectedTab());
+            selectedPage.setVisible(true);
+        });
+
+        add(actions, tabs, pages);
     }
 
     private void setItemsToGrid() {
-        grid.setItems(companyService.getAll());
+        companyGrid.setItems(companyService.getAll());
+        employeeGrid.setItems(employeeService.getAll());
     }
 }
