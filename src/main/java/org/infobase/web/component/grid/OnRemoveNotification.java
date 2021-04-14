f643c563077b7dd4546454ec2efb8b1f2c668471
konianzero <konianzero@gmail.com>
2021-04-14 11:43:21 +0300

    Add the ability to add a message to a notification: - When deleting a company show a notification that all employees of the company will also be deleted

--- src/main/java/org/infobase/web/component/grid/OnRemoveNotification.java
+++ src/main/java/org/infobase/web/component/grid/OnRemoveNotification.java
@@ -1,18 +1,18 @@
 package org.infobase.web.component.grid;
 
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 
 public interface OnRemoveNotification {
 
-    default void preRemoveNotification(Runnable deleteCallback) {
+    default void preRemoveNotification(Runnable deleteCallback, String notificationMessage) {
         Button yes = new Button("Да");
         Button no = new Button("Нет");
         HorizontalLayout btnLayout = new HorizontalLayout(yes, no);
         btnLayout.setSpacing(true);
-        Span text = new Span("Подтвердите удаление");
+        Label text = new Label(notificationMessage + " Подтвердите удаление.");
 
         Notification notification = new Notification(text, btnLayout);
         notification.setPosition(Notification.Position.MIDDLE);
