ccd370a8fbf864926ba01894464f880b951a8b6a
konianzero <konianzero@gmail.com>
2021-04-15 13:28:57 +0300

    Refactor SearchPane, add lombok to it

--- src/main/java/org/infobase/web/view/SearchPanel.java
+++ src/main/java/org/infobase/web/view/SearchPanel.java
@@ -5,73 +5,99 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
+import lombok.Getter;
 import org.infobase.web.component.CompanyComboBox;
 import org.infobase.web.component.LocalizedDatePicker;
 
 @SpringComponent
 @UIScope
+@Getter
 public class SearchPanel {
 
-    final ComboBox<String> searchColumn;
-    final TextField searchText;
-    final LocalizedDatePicker birthDatePicker;
-    final CompanyComboBox<String> companyPicker;
+    private final ComboBox<String> columnToSearch;
+    private final TextField textToSearch;
+    private final LocalizedDatePicker birthDatePicker;
+    private final CompanyComboBox<String> companyPicker;
 
-    final HorizontalLayout filter;
+    private final HorizontalLayout filters;
 
     public SearchPanel() {
-        searchColumn = new ComboBox<>();
-        searchColumn.setPlaceholder("Выберите поле");
-        searchColumn.setRequired(true);
-        searchColumn.setClearButtonVisible(true);
+        columnToSearch = new ComboBox<>();
+        columnToSearch.setPlaceholder("Выберите поле");
+        columnToSearch.setRequired(true);
+        columnToSearch.setClearButtonVisible(true);
 
-        searchText = new TextField("", "Поиск");
+        textToSearch = new TextField("", "Поиск");
+        textToSearch.setClearButtonVisible(true);
         birthDatePicker = new LocalizedDatePicker();
         companyPicker = new CompanyComboBox<>();
 
-        setSearchTextVisibleAndEnable(true);
-
-        filter = new HorizontalLayout(searchColumn, searchText, birthDatePicker, companyPicker);
+        filters = new HorizontalLayout(columnToSearch, textToSearch, birthDatePicker, companyPicker);
     }
 
-    public void setSearchColumnVisibleAndEnable(boolean bool) {
-        searchColumn.setEnabled(bool);
-        searchColumn.setVisible(bool);
+    public void showSearchColumn(boolean bool) {
+        setSearchColumnVisibleAndEnable(bool);
 
         if (!bool) {
-            setSearchTextVisibleAndEnable(true);
-            setDatePickerVisibleAndEnable(false);
-            setCompanyPickerVisibleAndEnable(false);
+            showSearchText();
+        } else {
+            textToSearch.clear();
         }
     }
 
-    public void setDatePickerVisibleAndEnable(boolean bool) {
+    public void showDatePicker() {
+        setDatePickerVisibleAndEnable(true);
+        birthDatePicker.clear();
+
+        setSearchTextVisibleAndEnable(false);
+        setCompanyPickerVisibleAndEnable(false);
+    }
+
+    public void showSearchText() {
+        setSearchTextVisibleAndEnable(true);
+        textToSearch.clear();
+
+        setDatePickerVisibleAndEnable(false);
+        setCompanyPickerVisibleAndEnable(false);
+    }
+
+    public void showCompanyPicker() {
+        setCompanyPickerVisibleAndEnable(true);
+        companyPicker.clear();
+
+        setSearchTextVisibleAndEnable(false);
+        setDatePickerVisibleAndEnable(false);
+    }
+
+    private void setSearchColumnVisibleAndEnable(boolean bool) {
+        columnToSearch.setEnabled(bool);
+        columnToSearch.setVisible(bool);
+    }
+
+    private void setDatePickerVisibleAndEnable(boolean bool) {
         birthDatePicker.setEnabled(bool);
         birthDatePicker.setVisible(bool);
-
-        if (bool) {
-            setSearchTextVisibleAndEnable(false);
-            setCompanyPickerVisibleAndEnable(false);
-        }
     }
 
-    public void setSearchTextVisibleAndEnable(boolean bool) {
-        searchText.setEnabled(bool);
-        searchText.setVisible(bool);
-
-        if (bool) {
-            setDatePickerVisibleAndEnable(false);
-            setCompanyPickerVisibleAndEnable(false);
-        }
+    private void setSearchTextVisibleAndEnable(boolean bool) {
+        textToSearch.setEnabled(bool);
+        textToSearch.setVisible(bool);
     }
 
-    public void setCompanyPickerVisibleAndEnable(boolean bool) {
+    private void setCompanyPickerVisibleAndEnable(boolean bool) {
         companyPicker.setEnabled(bool);
         companyPicker.setVisible(bool);
+    }
 
-        if (bool) {
-            setSearchTextVisibleAndEnable(false);
-            setDatePickerVisibleAndEnable(false);
-        }
+    public void disabledSearchInput() {
+        textToSearch.setEnabled(false);
+        birthDatePicker.setEnabled(false);
+        companyPicker.setEnabled(false);
+    }
+
+    public void clearSearchInput() {
+        textToSearch.clear();
+        birthDatePicker.clear();
+        companyPicker.clear();
     }
 }
