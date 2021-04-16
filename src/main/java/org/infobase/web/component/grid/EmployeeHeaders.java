2c407547fd10e522d5c755c03175f304418c359f
konianzero <konianzero@gmail.com>
2021-04-16 14:00:57 +0300

    Small fix

--- src/main/java/org/infobase/web/component/grid/EmployeeHeaders.java
+++ src/main/java/org/infobase/web/component/grid/EmployeeHeaders.java
@@ -6,21 +6,11 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum EmployeeHeaders {
-    ID {
-        @Override public String getHeader() { return "ID"; }
-    },
-    FULL_NAME {
-        @Override public String getHeader() { return "ФИО"; }
-    },
-    BIRTH_DATE {
-        @Override public String getHeader() { return "Дата Рождения"; }
-    },
-    EMAIL {
-        @Override public String getHeader() { return "Электронная почта"; }
-    },
-    COMPANY {
-        @Override public String getHeader() { return "Компания"; }
-    };
+    ID          { @Override public String getHeader() { return "ID";                } },
+    FULL_NAME   { @Override public String getHeader() { return "ФИО";               } },
+    BIRTH_DATE  { @Override public String getHeader() { return "Дата Рождения";     } },
+    EMAIL       { @Override public String getHeader() { return "Электронная почта"; } },
+    COMPANY     { @Override public String getHeader() { return "Компания";          } };
 
     public abstract String getHeader();
 
