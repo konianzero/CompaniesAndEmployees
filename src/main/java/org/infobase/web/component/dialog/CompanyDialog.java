bbffbe07cf2c5af2c78850d7548ab867174695f9
konianzero <konianzero@gmail.com>
2021-04-16 11:24:33 +0300

    Small fix

--- src/main/java/org/infobase/web/component/dialog/CompanyDialog.java
+++ src/main/java/org/infobase/web/component/dialog/CompanyDialog.java
@@ -2,7 +2,6 @@ package org.infobase.web.component.dialog;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -20,44 +19,42 @@ import org.infobase.web.component.grid.OperationNotification;
 public class CompanyDialog extends Dialog implements OperationNotification {
 
     private final CompanyService companyService;
-    private final BeanValidationBinder<Company> binder;
+    private final BeanValidationBinder<Company> binder = new BeanValidationBinder<>(Company.class);
 
     private Company company;
     private Runnable onSave;
 
-    private Label nameLabel = new Label("Название");
-    private Label tinLabel = new Label("ИНН");
-    private Label addressLabel = new Label("Адрес");
-    private Label phoneLabel = new Label("Телефон");
-
-    private TextField name = new TextField("");
-    private TextField tin = new TextField("");
-    private TextField address = new TextField("");
-    private TextField phoneNumber = new TextField("");
+    private TextField name = new TextField("Название");
+    private TextField tin = new TextField("ИНН");
+    private TextField address = new TextField("Адрес");
+    private TextField phoneNumber = new TextField("Телефон");
     private Button saveBtn = new Button("Сохранить");
     private Button cancelBtn = new Button("Отменить");
 
     public CompanyDialog(CompanyService companyService) {
         this.companyService = companyService;
 
-        binder = new BeanValidationBinder<>(Company.class);
-        binder.forField(phoneNumber)
-              .withValidator(
-                      new RegexpValidator(
-                              "Указанный номер не соответствует формату",
-                              "^(8|\\+7) \\(\\d{3}\\) \\d{3}-\\d{2}-\\d{2}$"
-                      )
-              )
-              .bind(Company::getPhoneNumber, Company::setPhoneNumber);
-        binder.bindInstanceFields(this);
+        binding();
 
         saveBtn.addClickListener(e -> save());
         cancelBtn.addClickListener(e -> close());
 
-        VerticalLayout inputLayout = new VerticalLayout(nameLabel, name, tinLabel, tin, addressLabel, address, phoneLabel, phoneNumber);
+        VerticalLayout inputLayout = new VerticalLayout(name, tin, address,  phoneNumber);
         HorizontalLayout actionLayout = new HorizontalLayout(saveBtn, cancelBtn);
         VerticalLayout layout = new VerticalLayout(inputLayout, actionLayout);
         add(layout);
+    }
+
+    public void binding() {
+        binder.forField(phoneNumber)
+                .withValidator(
+                        new RegexpValidator(
+                                "Указанный номер не соответствует формату",
+                                "^(8|\\+7) \\(\\d{3}\\) \\d{3}-\\d{2}-\\d{2}$"
+                        )
+                )
+                .bind(Company::getPhoneNumber, Company::setPhoneNumber);
+        binder.bindInstanceFields(this);
     }
 
     public void setOnSave(Runnable onSave) {
--- src/main/java/org/infobase/web/component/dialog/EmployeeDialog.java
+++ src/main/java/org/infobase/web/component/dialog/EmployeeDialog.java
@@ -3,7 +3,6 @@ package org.infobase.web.component.dialog;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dialog.Dialog;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -24,19 +23,14 @@ import java.util.List;
 public class EmployeeDialog extends Dialog implements OperationNotification {
 
     private final EmployeeService employeeService;
-    private final BeanValidationBinder<EmployeeTo> binder;
+    private final BeanValidationBinder<EmployeeTo> binder = new BeanValidationBinder<>(EmployeeTo.class);
 
     private EmployeeTo employeeTo;
     private Runnable onSave;
 
-    private Label nameLabel = new Label("ФИО");
-    private Label birthdayLabel = new Label("Дата Рождения");
-    private Label emailLabel = new Label("Электронная почта");
-    private Label companyLabel = new Label("Компания");
-
-    private TextField name = new TextField("");
+    private TextField name = new TextField("ФИО");
     private LocalizedDatePicker birthDate = new LocalizedDatePicker();
-    private TextField email = new TextField("");
+    private TextField email = new TextField("Электронная почта");
     private ComboBox<String> companyName = new ComboBox<>();
     private Button saveBtn = new Button("Сохранить");
     private Button cancelBtn = new Button("Отменить");
@@ -44,23 +38,29 @@ public class EmployeeDialog extends Dialog implements OperationNotification {
     public EmployeeDialog(EmployeeService employeeService) {
         this.employeeService = employeeService;
 
-        binder = new BeanValidationBinder<>(EmployeeTo.class);
-        binder.forField(birthDate)
-              .asRequired("Пожалуйста, выберите дату")
-              .bind(EmployeeTo::getBirthDate, EmployeeTo::setBirthDate);
-        binder.forField(email)
-              .asRequired("Пожалуйста, укажите электронный адрес")
-              .withValidator(new EmailValidator("Не соответствует формату адреса электронной почты"))
-              .bind(EmployeeTo::getEmail, EmployeeTo::setEmail);
-        binder.bindInstanceFields(this);
+        birthDate.setLabel("Дата Рождения");
+        companyName.setLabel("Компания");
+
+        binding();
 
         saveBtn.addClickListener(e -> save());
         cancelBtn.addClickListener(e -> close());
 
-        VerticalLayout inputLayout = new VerticalLayout(nameLabel, name, birthdayLabel, birthDate, emailLabel, email, companyLabel, companyName);
+        VerticalLayout inputLayout = new VerticalLayout(name, birthDate, email, companyName);
         HorizontalLayout actionLayout = new HorizontalLayout(saveBtn, cancelBtn);
         VerticalLayout layout = new VerticalLayout(inputLayout, actionLayout);
         add(layout);
+    }
+
+    public void binding() {
+        binder.forField(birthDate)
+                .asRequired("Пожалуйста, выберите дату")
+                .bind(EmployeeTo::getBirthDate, EmployeeTo::setBirthDate);
+        binder.forField(email)
+                .asRequired("Пожалуйста, укажите электронный адрес")
+                .withValidator(new EmailValidator("Не соответствует формату адреса электронной почты"))
+                .bind(EmployeeTo::getEmail, EmployeeTo::setEmail);
+        binder.bindInstanceFields(this);
     }
 
     public void setItemsToCompanyComboBox(List<String> companiesNames) {
