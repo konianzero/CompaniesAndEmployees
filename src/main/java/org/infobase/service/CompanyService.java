4936d108feaa7cc02a78666bdd770bbb3e94a64c
konianzero <konianzero@gmail.com>
2021-04-16 17:22:22 +0300

    Add doc comments to services

--- src/main/java/org/infobase/service/CompanyService.java
+++ src/main/java/org/infobase/service/CompanyService.java
@@ -10,16 +10,30 @@ import java.util.stream.Collectors;
 import org.infobase.model.Company;
 import org.infobase.dao.impl.CompanyDaoImpl;
 
+/**
+ * Класс бизнес логики для сущностей компаний
+ */
 @Service
 @Slf4j
 public class CompanyService {
 
+    /** Класс доступа к данным компаний */
     private final CompanyDaoImpl companyDaoImpl;
 
+    /**
+     * Создание класса бизнес логики для сущностей компаний
+     * @param companyDaoImpl Класс доступа к данным компаний
+     */
     public CompanyService(CompanyDaoImpl companyDaoImpl) {
         this.companyDaoImpl = companyDaoImpl;
     }
 
+    /**
+     * Сохранение сущности новой компании или обновление существующей
+     * @param company сущность компании
+     * @return 0 - если операция не выполнена;
+     *         число больше нуля если операция прошла успешно
+     */
     public int saveOrUpdate(Company company) {
         if (company.isNew()) {
             log.info("Save {}", company);
@@ -29,16 +43,29 @@ public class CompanyService {
         return companyDaoImpl.update(company);
     }
 
+    /**
+     * Получить сущность компании по ID
+     * @param id ID сущности компании
+     * @return сущность компании
+     */
     public Company get(int id) {
         log.info("Get company with id:{}", id);
         return companyDaoImpl.getById(id);
     }
 
+    /**
+     * Получение списка сущностей всех компаний
+     * @return список сущностей все компаний
+     */
     public List<Company> getAll() {
         log.info("Get all companies");
         return companyDaoImpl.getAll();
     }
 
+    /**
+     * Получить имена всех компаний
+     * @return список имен всех компаний
+     */
     public List<String> getNames() {
         log.info("Get all companies names");
         return companyDaoImpl.getAll()
@@ -47,11 +74,22 @@ public class CompanyService {
                              .collect(Collectors.toList());
     }
 
+    /**
+     * Поиск сущностей компаний
+     * @param textToSearch текст поиска
+     * @return список сущностей компаний подходящих по параметрам поиска
+     */
     public List<Company> search(String textToSearch) {
         log.info("Search companies with \"{}\"", textToSearch);
         return companyDaoImpl.search(textToSearch);
     }
 
+    /**
+     * Удаление сущности компании
+     * @param id ID сущности компании
+     * @return true - если операция выполнена успешно;
+     *         false - если операция не выполнена
+     */
     public boolean delete(int id) {
         log.info("Delete company with id:{}", id);
         return companyDaoImpl.delete(id);
--- src/main/java/org/infobase/service/EmployeeService.java
+++ src/main/java/org/infobase/service/EmployeeService.java
@@ -14,18 +14,34 @@ import org.infobase.util.EmployeeUtil;
 
 import org.infobase.web.component.grid.EmployeeHeaders;
 
+/**
+ * Класс бизнес логики для сущностей сотрудников
+ */
 @Service
 @Slf4j
 public class EmployeeService {
 
+    /** Класс доступа к данным сотрудников */
     private final EmployeeDaoImpl employeeDaoImpl;
+    /** Класс доступа к данным компаний */
     private final CompanyDaoImpl companyDaoImpl;
 
+    /**
+     * Создание класс бизнес логики для сущностей сотрудников
+     * @param employeeDaoImpl Класс доступа к данным сотрудников
+     * @param companyDaoImpl Класс доступа к данным компаний
+     */
     public EmployeeService(EmployeeDaoImpl employeeDaoImpl, CompanyDaoImpl companyDaoImpl) {
         this.employeeDaoImpl = employeeDaoImpl;
         this.companyDaoImpl = companyDaoImpl;
     }
 
+    /**
+     * Сохранение сущности нового сотрудника или обновление существующей
+     * @param employeeTo сущность сотрудника для отображения в интерфейсе
+     * @return 0 - если операция не выполнена;
+     *         число больше нуля если операция прошла успешно
+     */
     public int saveOrUpdate(EmployeeTo employeeTo) {
         Employee employee = EmployeeUtil.createEntity(employeeTo);
         employee.setCompany(companyDaoImpl.getByName(employeeTo.getCompanyName()));
@@ -38,21 +54,42 @@ public class EmployeeService {
         return employeeDaoImpl.update(employee);
     }
 
+    /**
+     * Получить сущность сотрудника по ID
+     * @param id ID сущности сотрудника
+     * @return сущность сотрудника для отображения в интерфейсе
+     */
     public EmployeeTo get(int id) {
         log.info("Get employee with id:{}", id);
         return EmployeeUtil.createTo(employeeDaoImpl.get(id));
     }
 
+    /**
+     * Получение списка сущностей всех сотрудников
+     * @return список сущностей(для отображения в интерфейсе) всех сотрудников
+     */
     public List<EmployeeTo> getAll() {
         log.info("Get all employees");
         return EmployeeUtil.createToList(employeeDaoImpl.getAll());
     }
 
+    /**
+     * Поиск сущностей сотрудников
+     * @param columnName название колонки таблицы для поиска
+     * @param textToSearch текст поиска
+     * @return список сущностей(для отображения в интерфейсе) сотрудников подходящих по параметрам поиска
+     */
     public List<EmployeeTo> search(EmployeeHeaders columnName, String textToSearch) {
         log.info("Search employees by field '{}' with \"{}\"", columnName.getHeader(), textToSearch);
         return EmployeeUtil.createToList(employeeDaoImpl.search(columnName, textToSearch));
     }
 
+    /**
+     * Удаление сущности сотрудника
+     * @param id ID сущности сотрудника
+     * @return true - если операция выполнена успешно;
+     *         false - если операция не выполнена
+     */
     public boolean delete(int id) {
         log.info("Delete employee with id:{}", id);
         return employeeDaoImpl.delete(id);
