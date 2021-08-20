3978a145dd45b77268ee173db599726e9138035d
konianzero <konianzero@gmail.com>
2021-08-20 13:36:55 +0300

    feat: Move logging config to application.yaml
    - fix: Small fixes

--- src/test/java/org/infobase/service/CompanyServiceTest.java
+++ src/test/java/org/infobase/service/CompanyServiceTest.java
@@ -5,13 +5,10 @@ import org.infobase.model.Company;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-
 import org.junit.jupiter.api.TestMethodOrder;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
 
 import java.util.List;
 
--- src/test/java/org/infobase/service/EmployeeServiceTest.java
+++ src/test/java/org/infobase/service/EmployeeServiceTest.java
@@ -5,12 +5,10 @@ import org.infobase.to.EmployeeTo;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-
 import org.junit.jupiter.api.TestMethodOrder;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
 
 import java.util.List;
 
