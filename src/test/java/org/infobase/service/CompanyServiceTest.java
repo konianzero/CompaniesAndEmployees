7d93c924e4f76aef41bb3845fe20e1417be6eea6
konianzero <konianzero@gmail.com>
2022-10-24 18:00:37 +0300

    feat: Add @Transactional to tests

--- src/test/java/org/infobase/service/CompanyServiceTest.java
+++ src/test/java/org/infobase/service/CompanyServiceTest.java
@@ -7,10 +7,7 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
@@ -21,46 +18,26 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 @ActiveProfiles("test")
 @SpringBootTest
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Transactional
 @Testcontainers
 class CompanyServiceTest {
-
-    @Container
-    private static PostgreSQLContainer<?> postgresDB = new PostgreSQLContainer<>("postgres:10").withDatabaseName("test_db");
-
-    @DynamicPropertySource
-    public static void properties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url",postgresDB::getJdbcUrl);
-        registry.add("spring.datasource.username", postgresDB::getUsername);
-        registry.add("spring.datasource.password", postgresDB::getPassword);
-    }
-
-    @Test
-    @Order(1)
-    @DisplayName("start container")
-    void test() {
-        Assertions.assertTrue(postgresDB.isRunning());
-    }
 
     @Autowired
     private CompanyService companyService;
 
     @Test
-    @Order(2)
     void get() {
         Company actual = companyService.get(COMPANY_1_ID);
         COMPANY_MATCHER.assertMatch(actual, COMPANY_1);
     }
 
     @Test
-    @Order(3)
     void getAll() {
         List<Company> actual = companyService.getAll();
         COMPANY_MATCHER.assertMatch(actual, ALL_COMPANIES);
     }
 
     @Test
-    @Order(4)
     void save() {
         Company newCompany = getNew();
         int id = companyService.saveOrUpdate(newCompany);
@@ -69,7 +46,6 @@ class CompanyServiceTest {
     }
 
     @Test
-    @Order(5)
     void update() {
         Company updated = getUpdated();
         companyService.saveOrUpdate(updated);
@@ -77,7 +53,6 @@ class CompanyServiceTest {
     }
 
     @Test
-    @Order(6)
     void delete() {
         companyService.delete(COMPANY_1_ID);
         assertNull(companyService.get(COMPANY_1_ID));
--- src/test/java/org/infobase/service/EmployeeServiceTest.java
+++ src/test/java/org/infobase/service/EmployeeServiceTest.java
@@ -8,10 +8,7 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
@@ -21,26 +18,9 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 @ActiveProfiles("test")
 @SpringBootTest
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Transactional
 @Testcontainers
 class EmployeeServiceTest {
-
-    @Container
-    private static PostgreSQLContainer<?> postgresDB = new PostgreSQLContainer<>("postgres:10").withDatabaseName("test_db");
-
-    @DynamicPropertySource
-    public static void properties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url",postgresDB::getJdbcUrl);
-        registry.add("spring.datasource.username", postgresDB::getUsername);
-        registry.add("spring.datasource.password", postgresDB::getPassword);
-    }
-
-    @Test
-    @Order(1)
-    @DisplayName("start container")
-    void test() {
-        Assertions.assertTrue(postgresDB.isRunning());
-    }
 
     @Autowired
     private EmployeeService employeeService;
@@ -48,21 +28,18 @@ class EmployeeServiceTest {
     private EmployeeMapper mapper;
 
     @Test
-    @Order(2)
     void get() {
         EmployeeTo actual = employeeService.get(EMPLOYEE_1_ID);
         EMPLOYEE_TO_MATCHER.assertMatch(actual, mapper.toEmployeeTo(EMPLOYEE_1));
     }
 
     @Test
-    @Order(3)
     void getAll() {
         List<EmployeeTo> actual = employeeService.getAll();
         EMPLOYEE_TO_MATCHER.assertMatch(actual, mapper.toEmployeeToList(ALL_EMPLOYEES));
     }
 
     @Test
-    @Order(4)
     void save() {
         EmployeeTo newEmployeeTo = mapper.toEmployeeTo(getNew());
         int id = employeeService.saveOrUpdate(newEmployeeTo);
@@ -71,7 +48,6 @@ class EmployeeServiceTest {
     }
 
     @Test
-    @Order(5)
     void update() {
         EmployeeTo updatedTo = mapper.toEmployeeTo(getUpdated());
         employeeService.saveOrUpdate(updatedTo);
@@ -79,7 +55,6 @@ class EmployeeServiceTest {
     }
 
     @Test
-    @Order(6)
     void delete() {
         employeeService.delete(EMPLOYEE_1_ID);
         assertNull(employeeService.get(EMPLOYEE_1_ID));
